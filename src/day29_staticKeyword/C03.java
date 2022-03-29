2e518480b4e2b5b3ec6474f953db1f2e86449d96
Cevdet Gulser <c.gulser86@gmail.com>
2022-03-29 22:38:04 +0200

    guncelleme

--- src/day29_staticKeyword/C03.java
+++ src/day29_staticKeyword/C03.java
@@ -1,2 +1,16 @@
-package day29_staticKeyword;public class C03 {
+package day29_staticKeyword;
+public class C03 {
+
+
+    static int sayi=5;
+
+
+
+    public static void artirma(){
+
+        sayi++;
+
+        System.out.println("sayinin su andaki degeri : " + C03.sayi);
+
+    }
 }
--- src/day29_staticKeyword/C05.java
+++ src/day29_staticKeyword/C05.java
@@ -1,2 +1,9 @@
-package day29_staticKeyword;public class C05 {
+package day29_staticKeyword;
+
+public class C05 {
+
+    public static void eksiltme (){
+        C03.sayi--;
+        System.out.println(C03.sayi);
+    }
 }
